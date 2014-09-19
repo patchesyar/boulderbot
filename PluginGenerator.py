6eb1bb76ae8b1512f6d5896142ea44e97c7125ee
John Lavoie <johnlavoi@gmail.com>
2014-09-18 20:50:23 -0400

    Alpha commit of PluginGenerator. Version 0.1 Creates Plugins for you, currently only the call/reply method works (and the compiler technically). TODO: make another menu for advanced premade fuctions (raffles, request queues, whatever else) and the custom plugin creator (basically word processor for the plugin.)

---
+++ PluginGenerator.py
@@ -0,0 +1,68 @@
+#testing file i/o for python
+#testing to see about a .py->.py code generator
+#this will be used for a command generator for boulderbot!
+
+import os
+import time
+
+def makePlugin():
+    np =input ("What is the name of the new plugin? ")
+    print(os.listdir())
+    time.sleep(1)
+    os.chdir("C:\\Python34\\twitchy-master (1)\\Boulderbot master files\\plugins")
+    print(os.listdir())
+    time.sleep(1)
+    os.mkdir(np)
+    os.chdir(np)
+    print(os.listdir())
+    file = open("plugin.py", "w+")
+    file.write("from plugins.BasePlugin import BasePlugin\n")
+    file.write("class "+np+"Plugin(BasePlugin):\n")
+    runMenu(file, np)
+
+def runMenu(file, np):
+    keepSwimming=True
+    commandTriggers=[]
+    while(keepSwimming):
+        print("Select a command:")
+        print("1. Call/Reply Generator")
+        print("2. Advanced Commands")
+        print("3. Custom Commands (Requires knowledge of Python)")
+        print("4. Quit and compile Plugin")
+        s=int(input("=> "))
+        if s==1:
+            newCommand=callGen(file)
+            commandTriggers.append(newCommand)
+        elif s==2:
+            #advMenu(file)
+            pass
+        elif s==3:
+            #customProcessor
+            pass
+        elif s==4:
+            keepSwimming=False
+        else:
+            print("Invalid input. Enter a number in the range of 1-4")
+    print(commandTriggers)
+    compilePlugin(file, commandTriggers, np)
+    
+def callGen(file):
+    command= input("Input the name of the command trigger: ")
+    reply= input("Input the reply: ")
+    print(command)
+    file.write("\tdef %sHandler (self, nick, commandArg)\n" %(command))
+    file.write("\t\tprint('!%s called by '+nick)\n" %(command))
+    file.write("\t\tself.sendMessage('%s')\n\n" %(reply))
+    return(command)
+
+def compilePlugin (file, commandTriggers, np):
+    file.write("\tdef __init__(self, twitchy):\n")
+    file.write("\t\tsuper("+np+"Plugin, self).__init__(twitchy)\n\n")
+    print("writing commands")
+    print(commandTriggers)
+    for command in commandTriggers:
+        print("writing the %s plugin" %command)
+        file.write("\t\tself.registerCommand('%s', self.%sHandler)\n" %(command, command))
+
+#main
+makePlugin()
---
+++ TODO.md
@@ -0,0 +1,6 @@
+TODO
+=======
+
+- Check for Python 3 compatibility  
+ - Started, aiming for same-source strategy
+- Confirm Linux compatibility
